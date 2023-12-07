b4b6a042eacdfa8c53868258606a7a427d06fb5d
Anna <anjanadvorna@gmail.com>
2023-12-07 12:47:09 +0200

    Update Container.styled.jsx

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled';
 
 export const ContainerContent = styled.div`
-    max-width: 1200px;
+    /* max-width: 1200px; */
     padding-left: 20px;
     padding-right: 20px;
-    margin-left: auto;
-    margin-right: auto;    
+    /* margin-left: auto;
+    margin-right: auto;     */
 `
